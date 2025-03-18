66e5b48ef97f8738ddbf9e229c877e558badb345
fraaaaaa997 <frn.marinelli@icloud.com>
2025-03-18 16:21:54 +0100

    Add files via upload

--- giorni.py
+++ giorni.py
@@ -3,3 +3,13 @@ giorno = int(input("Inserisci un numero:"))
 giorni = ["Lunedì", "Martedì", "Mercoledì", "Giovedì", "Venerdì", "Sabato", "Domenica"]
 
 print(giorni[giorno - 1])
+
+#########################################################################################
+
+giorno = int(input("Inserisci un numero:"))
+
+giorni ="Lunedì   "+"Martedì  "+"Mercoledì"+"Giovedì  "+"Venerdì  "+"Sabato   "+"Domenica "
+
+p = (giorno -1) * 9
+
+print(giorni[p:p+9])
